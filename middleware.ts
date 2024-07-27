d5287785e2160d5a7f00943de584b90d14fe34b9
Joshua Hudson <joshua.hudson@eden.co.uk>
2024-07-27 10:59:18 +0100

    Add midleware back

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    if((new Date() < new Date('2024-07-27 15:00:00'))) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: '/invite/:id*',
+}

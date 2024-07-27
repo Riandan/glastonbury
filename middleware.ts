f17725e6b0ea35a1d3e65c7cd78dca2306bfe92b
Joshua Hudson <joshua.hudson@eden.co.uk>
2024-07-27 14:52:05 +0100

    reduce timer

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    if((new Date() < new Date('2024-07-27 15:00:00'))) {
+    if((new Date() < new Date('2024-07-27 14:50:00'))) {
         return NextResponse.redirect(new URL('/', request.url))
     }
 }
